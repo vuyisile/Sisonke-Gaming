464e749ac22215d3d52efd95405c68d4402e2cc3
Florah <f.n.weni@gmail.com>
2016-02-06 03:38:15 -0500

    hello

---
+++ Sisonke Gaming/soildation.py
@@ -0,0 +1,11 @@
+import random, pygame, sys
+from pygame.locals import *
+
+FPS = 15
+WINDOWWIDTH = 1200
+WINDOWHEIGHT = 800
+CELLSIZE = 20
+assert WINDOWWIDTH % CELLSIZE == 0, "Window width must be a multiple of cell size."
+assert WINDOWHEIGHT % CELLSIZE == 0, "Window height must be a multiple of cell size."
+CELLWIDTH = int(WINDOWWIDTH / CELLSIZE)
+CELLHEIGHT = int(WINDOWHEIGHT / CELLSIZE)
